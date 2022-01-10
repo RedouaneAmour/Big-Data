1173c49894b78800ed210584df6f9721a0b72f62
Xaegon <62513370+Xaegon@users.noreply.github.com>
2022-01-10 19:25:34 +0100

    Add files via upload

---
+++ method_kmeans.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 23:08:23 2021
+
+@author: huguet
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import pandas as pd
+
+from scipy.io import arff
+from sklearn import cluster
+from sklearn import metrics
+from sklearn.cluster import KMeans 
+from scipy.spatial.distance import cdist 
+
+
+
+##################################################################
+# READ a data set (arff format)
+
+# Parser un fichier de données au format arff
+# datanp est un tableau (numpy) d'exemples avec pour chacun la liste 
+# des valeurs des features
+
+# Note 1 : 
+# dans les jeux de données considérés : 2 features (dimension 2 seulement)
+# t =np.array([[1,2], [3,4], [5,6], [7,8]]) 
+#
+# Note 2 : 
+# le jeu de données contient aussi un numéro de cluster pour chaque point
+# --> IGNORER CETTE INFORMATION ....
+#    2d-4c-no9.arff
+
+#Jeu de données en format arff
+path = './artificial/'
+databrut = arff.loadarff(open(path+"xclara.arff", 'r'))
+datanp = np.array([[x[0],x[1]] for x in databrut[0]])
+
+#Jeu de données en format txt
+#path = './new-data/'
+#filename = "d32.txt"
+#databrut = pd.read_csv(path+filename, sep=" ", encoding = "ISO-8859-1", skipinitialspace=True)
+#datanp = databrut.to_numpy()
+
+#print(databrut)
+#print(datanp)
+
+##################################################################
+# PLOT datanp (en 2D) - / scatter plot
+# Extraire chaque valeur de features pour en faire une liste
+# EX : 
+# - pour t1=t[:,0] --> [1, 3, 5, 7]
+# - pour t2=t[:,1] --> [2, 4, 6, 8]
+print("---------------------------------------")
+print("Affichage données initiales            ")
+f0 = datanp[:,0] # tous les élements de la première colonne
+f1 = datanp[:,1] # tous les éléments de la deuxième colonne
+
+plt.scatter(f0, f1, s=8)
+plt.title("Donnees initiales")
+plt.show()
+
+##################################################################
+# Run clustering method for a given number of clusters
+distortions = [] 
+inertias = [] 
+mapping1 = {} 
+mapping2 = {} 
+K = range(1,10) 
+
+for k in K: 
+    tps1 = time.time()
+    kmeanModel = KMeans(n_clusters=k).fit(datanp) 
+    kmeanModel.fit(datanp)     
+    tps2 = time.time()
+    distortions.append(sum(np.min(cdist(datanp, kmeanModel.cluster_centers_, 
+                      'euclidean'),axis=1)) / datanp.shape[0]) 
+    inertias.append(kmeanModel.inertia_) 
+    mapping1[k] = sum(np.min(cdist(datanp, kmeanModel.cluster_centers_, 
+                 'euclidean'),axis=1)) / datanp.shape[0] 
+    mapping2[k] = kmeanModel.inertia_ 
+    
+for key,val in mapping1.items(): 
+    print(str(key)+' : '+str(val))
+    
+plt.plot(K, distortions, 'bx-') 
+plt.xlabel('Values of K') 
+plt.ylabel('Distortion') 
+plt.title('The Elbow Method using Distortion') 
+plt.show() 
+
+for key,val in mapping2.items(): 
+    print(str(key)+' : '+str(val)) 
+
+plt.plot(K, inertias, 'bx-') 
+plt.xlabel('Values of K') 
+plt.ylabel('Inertia') 
+plt.title('The Elbow Method using Inertia') 
+plt.show() 
+
+print("temps d'execution kmeans ", tps2 - tps1, " secondes")
+
+########################################################################
+# TESTER PARAMETRES METHODE ET RECUPERER autres métriques
+##############
